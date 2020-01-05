d3fbfb32e3c99e46ed8ff040f885a4ecb31ab50e
Pedro Ivo Dias <pedryvo@gmail.com>
2020-01-05 03:39:32 -0300

    add users controller and tests

--- aula2/spec/factories/user.rb
+++ aula2/spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.first_name }
     level { Faker::Number.between(from: 1, to: 99) }
-    kind { %i[ knight wizard ].sample }
+    kind { %w[ knight wizard ].sample }
   end
 end
