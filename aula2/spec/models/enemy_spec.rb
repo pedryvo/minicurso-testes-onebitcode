9d08c0b9089dce69f713ebe605428664cfad3f42
Pedro Ivo Dias <pedryvo@gmail.com>
2020-01-09 17:57:39 -0300

    add last model test

--- aula2/spec/models/enemy_spec.rb
+++ aula2/spec/models/enemy_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Enemy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:enemy) { create(:enemy) }
+
+  it 'returns the correct current power' do
+    expect(enemy.current_power).to eq(enemy.power_base + ((enemy.level - 1) * enemy.power_step))
+  end
 end
--- aula2/spec/requests/enemies_spec.rb
+++ aula2/spec/requests/enemies_spec.rb
@@ -3,29 +3,21 @@ require 'rails_helper'
 RSpec.describe "Enemies", type: :request do
   describe 'PUT /enemies' do
     context 'when the enemy exists' do
+      let(:enemy) { create(:enemy) }
+      let(:enemy_attributes) { attributes_for(:enemy) }
+
+      before(:each) { put "/enemies/#{enemy.id}", params: enemy_attributes }
+
       it 'returns status code 200' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{enemy.id}", params: enemy_attributes
-        
         expect(response).to have_http_status(200)
       end
 
       it 'updates the enemy' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{enemy.id}", params: enemy_attributes
-
         expect(enemy.reload).to have_attributes(enemy_attributes)
       end
 
       it 'returns the enemy updated' do
-        enemy = create(:enemy)
-        enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{enemy.id}", params: enemy_attributes
-
-        json_response = JSON.parse(response.body)
-        expect(enemy.reload).to have_attributes(json_response.except('created_at', 'updated_at'))
+        expect(enemy.reload).to have_attributes(json.except('created_at', 'updated_at'))
       end
     end
 
@@ -60,10 +52,13 @@ RSpec.describe "Enemies", type: :request do
     context 'when the enemy does not exist' do
       it 'returns status code 404' do
         delete '/enemies/0'
-        expect(response.body).to match(/Couldn't find Enemy/)
+        expect(response).to have_http_status(404)
       end
 
-      it 'returns a not found message'
+      it 'returns a not found message' do
+        delete '/enemies/0'
+        expect(response.body).to match(/Couldn't find Enemy/)
+      end
     end
   end
 end
